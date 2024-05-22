6322113ecc951cb3a42f0964bff1cd6d0b5f1977
RaviChowdary97 <145635705+RaviChowdary97@users.noreply.github.com>
2024-05-22 11:48:01 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,22 +1,35 @@
 //2. get all the countries with a population of less than 2 lakhs  using filter function
-const xhr = new XMLHttpRequest();
+ var xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://restcountries.com/v3.1/all", true);
 
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+      xhr.send();
+      xhr.onload = function getCountriesWithLowPopulation() {
+        const apiUrl = "https://restcountries.com/v3.1/all";
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            const countriesWithLowPopulation = data.filter(
+              (country) => country.population < 200000
+            );
+            console.log("Countries with Population Less than 2 lakhs:");
+            countriesWithLowPopulation.forEach((people) => {
+              console.log(people.name.common);
+            });
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+      };
 
-xhr.send();
-xhr.onload = function getCountriesWithLowPopulation() {
-  const apiUrl = "https://restcountries.com/v3.1/all";
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const countriesWithLowPopulation = data.filter(
-        (country) => country.population < 200000
-      );
-      console.log("Countries with Population Less than 2 lakhs:");
-      console.log(countriesWithLowPopulation);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-};
-getCountriesWithLowPopulation();
+//OUTPUT
+Countries with Population Less than 2 lakhs:
+ Nauru
+ United States Virgin Islands
+ Niue
+ Palau
+ British Virgin Islands
+ Dominica
+ Antigua and Barbuda
+ Northern Mariana Islands
+ Saint Vincent and the Grenadines
+ Saint Lucia
