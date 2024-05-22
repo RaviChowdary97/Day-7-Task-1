a55014fce8b3a93a712e53baa98a10c4a9f0a203
RaviChowdary97 <145635705+RaviChowdary97@users.noreply.github.com>
2024-05-22 11:53:12 +0530

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,29 +1,41 @@
 //3. print the following details name, capital, flag using forEach function
-
 const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+      xhr.open("GET", "https://restcountries.com/v3.1/all", true);
 
-xhr.send();
-xhr.onload = function printCountryDetails() {
-  const apiUrl = "https://restcountries.com/v3.1/all";
+      xhr.send();
+      xhr.onload = function printCountryDetails() {
+        const apiUrl = "https://restcountries.com/v3.1/all";
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((country) => {
-        const name = country.name.common;
-        const capital = country.capital;
-        const flag = country.flags[0] || "N/A";
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            data.forEach((country) => {
+              const name = country.name.common;
+              const capital = country.capital;
+              const flag = country.flags.png || "N/A";
 
-        console.log(`Name: ${name}`);
-        console.log(`Capital: ${capital}`);
-        console.log(`Flag: ${flag}`);
-        console.log("--------------------");
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-};
-printCountryDetails();
+              console.log(`Name: ${name}`);
+              console.log(`Capital: ${capital}`);
+              console.log(`Flag: ${flag}`);
+              console.log("--------------------");
+            });
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+      };
+
+//OUTPUT
+Name: Moldova
+Capital: Chișinău
+Flag: https://flagcdn.com/w320/md.png
+ --------------------
+Name: United States
+Capital: Washington, D.C.
+Flag: https://flagcdn.com/w320/us.png
+--------------------
+Name: Mayotte
+Capital: Mamoudzou
+Flag: https://flagcdn.com/w320/yt.png
+
