cd0518b2c6f48d6b79c9797ba071e7a2d8560810
RaviChowdary97 <145635705+RaviChowdary97@users.noreply.github.com>
2024-05-22 10:28:46 +0530

    Update task.js

--- task.js
+++ task.js
@@ -1,23 +1,44 @@
 //1. get all countries from Asia continent/region using filter function
 
-const xhr = new XMLHttpRequest();
+ const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+      xhr.open("GET", "https://restcountries.com/v3.1/all");
 
-xhr.send();
-xhr.onload = function getCountriesFromAsia() {
-  const apiUrl = "https://restcountries.com/v3.1/all";
+      xhr.send();
+      xhr.onload = function getCountriesFromAsia() {
+        const apiUrl = "https://restcountries.com/v3.1/all";
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const asiaCountries = data.filter((country) => country.region === "Asia");
-      console.log("Countries in Asia:");
-      console.log(asiaCountries);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-};
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            const asiaCountries = data.filter(
+              (country) => country.region === "Asia"
+            );
 
-getCountriesFromAsia();
+            console.log("Countries in Asia:");
+            asiaCountries.forEach((country) => {
+              console.log(country.name.common);
+            });
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+      };
+
+//OUTPUT
+Countries in Asia:
+ Yemen
+ Malaysia
+ Sri Lanka
+ Kuwait
+ Syria
+ Jordan
+ Nepal
+Kazakhstan
+ Turkmenistan
+ Brunei
+ Bhutan
+ Azerbaijan
+United Arab Emirates
+ Uzbekistan
+ Hong Kong
